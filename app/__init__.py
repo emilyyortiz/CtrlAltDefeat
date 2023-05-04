395803af2c00dfae27f3bb6e609936d3b99f170e
brianwang30 <bwang30@stuy.edu>
2023-05-04 13:39:48 -0400

    living in your wifi

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,20 @@
-print("Hello World!")
+from flask import Flask, render_template, session, request, redirect
+import sqlite3
+import os #?
+#more imports for other files later
+
+app = Flask(__name__)
+
+#for later
+#app.secret_key - os.urandom(32)
+
+#Landing page, only page? 
+#i forgot difference between get/post!
+@app.route('/', methods=['GET', 'POST'])
+def placeholder():
+    return render_template('foo.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
