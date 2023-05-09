685d2919f8de0df951c8cbc89a69692a219f28c6
eortiz-30 <eortiz30@stuy.edu>
2023-05-09 14:06:24 -0400

    heyyy

--- app/api_handle.py
+++ app/api_handle.py
@@ -4,7 +4,7 @@ import os
 
 #musixmatch + some sound api +_wordcloud
 
-def music_api(search):
+def music_api(text):
     url = "https://api.musixmatch.com/ws/1.1/track.lyrics.get?"
     # track.search?"
     
@@ -25,3 +25,29 @@ def music_api(search):
     print(response)
 
 music_api("Blank Space")
+
+# takes in 
+# [{title:
+#   artist:
+#   lyrics:}]
+def music_search(text):
+    url = "https://api.musixmatch.com/ws/1.1/track.lyrics.get?"
+
+    path = os.path.dirname(os.path.realpath(__file__)) # path to current python file
+    
+    key = open(path + "/keys/key_api0", "r").read()
+    key = key.strip()
+
+    querystring = {
+            "apikey": key,
+            "commontrack_id": 56420303
+        }
+    # 56420303
+
+        response = requests.get(url, params=querystring).json()
+        print(response)
+
+    music_api("Blank Space")
+
+# we r supposed to show copyright lol
+# check musixmatch checklist
