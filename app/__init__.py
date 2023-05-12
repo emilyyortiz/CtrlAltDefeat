d7f7e60265f7eaf93c33f366e02506cd07388fc7
brianwang30 <bwang30@stuy.edu>
2023-05-12 14:10:18 -0400

    poop

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,7 @@ app.secret_key = os.urandom(32)
 def login():
     if 'username' in session: #when cookie work
     #if(False):
-        return redirect('/home')
+        return redirect('/home/pl')
     return render_template('login.html') #names subject to change
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -42,7 +42,7 @@ def make_account():
   create_user(request.form.get('username'), request.form.get('password')) #NEED method to create an entry in the table
   session['username'] = request.form['username']
   print(session['username'])
-  return redirect('/home') #/home or /
+  return redirect('/home/pl') #/home or /
   
 #this method actually verifies whether or not the login works
 @app.route('/auth', methods=['GET', 'POST'])
@@ -53,13 +53,21 @@ def authenticate():
   #if(False):
     return render_template('login.html', status='Incorrect login info')
   session['username'] = request.form['username']
-  return redirect('/home')
+  return redirect('/home/pl')
 
 #forgot to actually allow a logout (I think it was in the site map)
 @app.route('/logout')
 def logout():
   session.pop('username')
   return redirect('/')
+
+@app.route('/search', methods=['GET', 'POST'])
+def query():
+  query = request.form.get('songInput')
+  print(query)
+  if query == None:
+    query = "pl"
+  return redirect('/home/' + query)
 
 #TODO: 
 #Display wordcloud on playlist  OUTLINED
@@ -73,31 +81,35 @@ def logout():
 
 #MAIN ISSUES: 
 #should the cover be updated every time the page is viewed? Seems intensive on the API
-@app.route('/home', methods=['GET', 'POST'])
-def home():
+@app.route('/home/<query>', methods=['GET', 'POST'])
+def home(query):
   #maybe unnecessary? 
   if ('username' not in session):
     return redirect('/')
   return render_template('index.html')
 
-search = request.form['songInput']
+  print(query)
 
-'''
+
 #if song is default playlist, wordcloud update is necessary
-if(search == "pl"):
+if(query == "pl"):
   #gets the playlist for this user
-  pl = get_playlist(session['username'])
+  pl = user_playlist(session['username'])
+  print(pl)
+  print(len(pl))
 
-  #gets a dictionary with the number of instances of each word in a playlist: 
-  dic = {string w : int num} #future dictoinary containing all words in playlist
+  '''
+  #collective lyrics
+  lyrics = ""
 
   #INFORMATION IS ALL SAVED WITHIN THE PLAYLIST ARRAY OF DICTIONARIES
   #link = []  #string array of links to be used to play audio (if this becomes the used method)
   #artist = []  #string array of artist names to be displayed
-  current_song =  #necessary? pass in argument from form-action in HTML
+  current_song = pl[0] #necessary? pass in argument from form-action in HTML
 
   #get playlist method, load into array, find song information
-  for(song : pl):
+  
+  for(song in pl[0]):
 
     lyric = get_lyrics(song.get("name"))
     #get lyric method
