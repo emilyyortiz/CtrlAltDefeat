f1830dafbecf2ad0e97d46dd004c15747190750c
Yuki Feng <yuki.2102005@gmail.com>
2023-05-18 14:02:34 -0400

    added variables

--- app/static/js/foo.js
+++ app/static/js/foo.js
@@ -7,11 +7,8 @@
 
 // var dasbut = document.getElementById("b");
 // dasbut.addEventListener('click', ()=>{textChange()});
-
 var searchBtn = document.getElementById("b");
-
 var search = function() {
-    searchBtn.innerHTML = "poop";
+    searchBtn.innerHTML = "<span class='spinner-border spinner-border-sm' role='status' aria-hidden='true' id='spin'></span>";  
 }
-
 searchBtn.addEventListener('click', search);
