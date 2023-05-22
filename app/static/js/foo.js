497caf5ac3f7ecab3b5b0a5c75461beda1641309
Yuki Feng <yuki.2102005@gmail.com>
2023-05-22 13:51:03 -0400

    poop

--- app/static/js/foo.js
+++ app/static/js/foo.js
@@ -7,8 +7,14 @@
 
 // var dasbut = document.getElementById("b");
 // dasbut.addEventListener('click', ()=>{textChange()});
-var searchBtn = document.getElementById("b");
+var searchBtn = document.getElementById("searchBtn");
 var search = function() {
     searchBtn.innerHTML = "<span class='spinner-border spinner-border-sm' role='status' aria-hidden='true' id='spin'></span>";  
 }
 searchBtn.addEventListener('click', search);
+
+//testing parsing playlist array
+const playlistJSON = '["Poop", "Mr. Poop"]';
+const playlistArray = JSON.parse(playlistJSON);
+document.getElementById("playlist_0").innerHTML = playlistArray[0];
+console.log(playlistArray[0]);
